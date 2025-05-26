6c6f44b8122087de04febd68803a6f11afdc061d
Shaikshavali2004 <shaikshavali1019@gmail.com>
2025-05-26 19:52:00 +0530

    Python programs on list

---
+++ list.py
@@ -0,0 +1,23 @@
+"""fruits = ["Mango", "Apple", "Banana"]
+x,y,z=fruits
+print(x,y,z)"""
+
+
+fruits = ["Mango", "Grapes", "apple", "Cherry", "pomogranate"]
+fruits.append("strawberry")
+fruits.insert(2, "banana")
+fruits.remove("Grapes")
+print(fruits)
+yellow, violet, *red = fruits
+print(yellow)
+print(violet)
+print(red)
+
+
+
+
+"""
+fruits = ["Mango", "Grapres", "apple", "Cherry", "pomogranate"]
+for i in fruits:
+    print(i)"""
+
